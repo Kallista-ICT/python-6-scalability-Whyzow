e48479c1334b2f9caa0af4b375c3bf0b5f8c5521
Whyzow <wizoremainrb@gmail.com>
2025-05-13 07:21:53 +0000

    Submit

---
+++ greeting.py
@@ -0,0 +1,9 @@
+def hello(name):
+    print(f"Hellaur, {name}! Welcome to the game")
+
+person_1 = input("What's the first person's name? ")
+person_2 = input("What's the second person's name? ")
+
+hello(person_1)
+hello(person_2)
+
---
+++ monsters.py
@@ -0,0 +1,4 @@
+def create_monster(name, hp, attack):
+    
+    print(f"Monster created: {name}, HP: {hp}, Attack power: {attack}")
+
