9b6b68e0370c576d719e17e21a4a2c5d13c61c3b
Whyzow <wizoremainrb@gmail.com>
2025-05-19 15:08:08 +0000

    Submit

---
+++ assignment/monsters.py
@@ -0,0 +1,8 @@
+def create_monster():
+    name = input("What's the monster's name?")
+    hp = int(input("What's the monster's HP? "))
+    attack = int(input("What's the monster's attack power? "))
+    print(f"Monster created: {name}, HP: {hp}, Attack power: {attack}")
+
+monster_1 = print("Let's create the first monster"), create_monster()
+monster_2 = print("Let's create the second monster"), create_monster()
--- monsters.py
+++
@@ -1,4 +0,0 @@
-def create_monster(name, hp, attack):
-    
-    print(f"Monster created: {name}, HP: {hp}, Attack power: {attack}")
-
