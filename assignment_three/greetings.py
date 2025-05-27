899ad3fbfa206f6a00213ab72bab36cf4d4d0190
Whyzow <wizoremainrb@gmail.com>
2025-05-27 07:06:47 +0000

    Submit

---
+++ assignment_three/greetings.py
@@ -0,0 +1,21 @@
+def hello(name):
+    return f"Hello, {name}! Thank you for your assistance. Appreciate it."
+
+def good_morning(name):
+    return f"Good morning, {name}! Thank you for your assistance. Appreciate it."
+
+def good_afternoon(name):
+    return f"Good afternoon, {name}! Thank you for your assistance. Appreciate it."
+
+def good_evening(name):
+    return print(f"Good evening, {name}! Thank you for your assistance. Appreciate it.")
+
+def main():
+    print("This is a greeting module. Please run conversation.py")
+
+if __name__ == "__main__":
+    main()
+    
+
+
+
